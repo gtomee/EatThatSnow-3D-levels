e49df885d8af18d22b5879585d65c0a844825bfe
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2012-08-19 22:43:59 +0200

    Good textures for blocks

--- js/Main.js
+++ js/Main.js
@@ -25,12 +25,15 @@ function init() {
 	renderer = new THREE.WebGLRenderer({antialias: true});
 	renderer.setSize(document.body.clientWidth, document.body.clientHeight);
 	document.body.appendChild(renderer.domElement);
-	renderer.setClearColorHex(0xEEEEEE, 1.0);
+	renderer.setClearColorHex(0x50CCE1FF, 1.0);
 	renderer.clear();
 	
 	
 	// init camera
-	camera = new THREE.PerspectiveCamera(45, renderer.domElement.width/renderer.domElement.height, 1, 10000);
+	camera = new THREE.PerspectiveCamera(45, 
+		renderer.domElement.width/renderer.domElement.height, 
+		1, 
+		10000);
 	camera.position.z = 300;
 	
 	// init scene
@@ -38,18 +41,36 @@ function init() {
 	
 	
 	// loading materials
-	var material = new THREE.MeshLambertMaterial({
-		map: THREE.ImageUtils.loadTexture("data/img/middle.png")
+	var topMat = new THREE.MeshLambertMaterial({
+		map: THREE.ImageUtils.loadTexture("data/img/top.png")
 	});
+	
+	var bottomMat = new THREE.MeshLambertMaterial({
+		map: THREE.ImageUtils.loadTexture("data/img/bottom.png")
+	});
+	
+	var middleBottomMat = new THREE.MeshLambertMaterial({
+		map: THREE.ImageUtils.loadTexture("data/img/up_down.png")
+	});
+	
+	var materials = [
+		middleBottomMat,
+		middleBottomMat,
+		topMat,
+		bottomMat,
+		middleBottomMat,
+		middleBottomMat
+	];
+	
 	
 	for (var i = 0; i < 10; ++i) {
 		// init cube and add it to the scene
 		cube = new THREE.Mesh(
-			new THREE.CubeGeometry(50,50,50),
-			material
+			new THREE.CubeGeometry(50,50,50,3,3,3, materials),
+			new THREE.MeshFaceMaterial()
 			//new THREE.MeshBasicMaterial({color: 0x00FF00, opacity: 1})
 		);
-		cube.position.x = i*60;
+		cube.position.x = i*50;
 		cube.castShadow = true;
 		cube.receiveShadow = true;
 		
@@ -57,12 +78,12 @@ function init() {
 	}
 	
 	// init light
-	var light = new THREE.SpotLight();
-	light.position.set( 170, 330, -160 );
+	var light = new THREE.AmbientLight(0x999999);
+	light.position.set(0, 330, 160);
 	scene.add(light);
 	
 	// enable shadows on the renderer
-	renderer.shadowMapEnabled = true;
+	renderer.shadowMapEnabled = false;
 
 	// enable shadows for a light
 	light.castShadow = true;
@@ -81,7 +102,7 @@ function update() {
 function render(t) {
 	if (!paused) {
 	  camera.position.set(
-		Math.sin(t/1000)*300, 150, Math.cos(t/1000)*300);
+		Math.sin(t/1000)*900, 550, Math.cos(t/1000)*300);
 	  renderer.clear();
 	  camera.lookAt(scene.position);
 	  renderer.render(scene, camera);
